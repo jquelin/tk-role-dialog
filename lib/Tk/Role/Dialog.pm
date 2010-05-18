1ef4f3274c8e4812897ed0d460095b85966cdfe9
Jérôme Quelin <jquelin@gmail.com>
2010-05-18 17:40:11 +0200

    empty stub for module

---
+++ lib/Tk/Role/Dialog.pm
@@ -0,0 +1,39 @@
+use 5.010;
+use strict;
+use warnings;
+
+package Tk::Role::Dialog;
+# ABSTRACT: moose role for enhanced dialogs
+
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+You can look for information on this module at:
+
+=over 4
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Tk-Role-Dialog>
+
+=item * See open / report bugs
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Tk-Role-Dialog>
+
+=item * Git repository
+
+L<http://github.com/jquelin/tk-role-dialog>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Tk-Role-Dialog>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Tk-Role-Dialog>
+
+=back
+
